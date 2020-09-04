209cf60da3b89c9181052f3c23d7cb70685bc9e7
wxia <wxia@nvidia.com>
2020-09-04 10:14:16 -0700

    update stock tickers

--- nTest/nStockInterest.py
+++ nTest/nStockInterest.py
@@ -8,7 +8,7 @@ class nStockInterest:
         return self.sp500.get_sp500_tickers()
 
     def get_self_defined_stocks(self):
-        self_list = ['AAPL','ABT','AMZN','AMC','AMD','ATVI','AVGO','BA','BABA','BILI','BP','CAR','CCL','COST','CSCO','DELL','DIS','DOCU','ENS','EOG','FB','FL','GE','GILD','GME','GOOG','HTZ','INO','INTC','JD','JWN','KC','LMT','M','MRK','MRNA','MSFT','NFLX','NIO','NKE','NKLA','NTES','NVAX','NVDA','ORCL','PDD','PFE','PXLW','RCL','RTX','SBUX','SNAP','SPOT','TEAM','TME','TSLA','TSM','TWTR','UAL','UBER','WB','WORK','XLNX','ZM','ZNGA','HAS','SPG','CAKE','DOW','SBUX','RCL','IRBT','XLNX','GME','TTWO','NOK']
+        self_list = ['AAPL','ABT','AMZN','AMC','AMD','ATVI','AVGO','BA','BABA','BILI','BP','CAR','CCL','COST','CSCO','DELL','DIS','DOCU','ENS','EOG','FB','FL','GE','GILD','GME','GOOG','HTZ','INO','INTC','JD','JWN','KC','LMT','M','MRK','MRNA','MSFT','NFLX','NIO','NKE','NKLA','NTES','NVAX','NVDA','ORCL','PDD','PFE','RCL','RTX','SBUX','SNAP','SPOT','TEAM','TME','TSLA','TSM','TWTR','UAL','UBER','WB','WORK','XLNX','ZM','ZNGA','HAS','SPG','CAKE','DOW','SBUX','RCL','IRBT','XLNX','GME','TTWO','NOK','WMT','CRM','SQ','ZTO','BB','VIPS','GRPN','GRMN','SOGO','GPRO']
         return self_list
 
 if __name__ == '__main__':
